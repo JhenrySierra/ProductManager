8fc6cba33ff3f821db98af4c51c4a9e5360cfcd7
Jhenry Sierra <jhenrims@gmail.com>
2023-06-28 20:20:02 -0400

    Implemented the Carts system to manage the carts

--- src/app.js
+++ src/app.js
@@ -1,29 +1,23 @@
 const express = require('express');
-const path = require('path');
-const ProductManager = require('./productManagement');
-
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
+// Load products and carts data from JSON files
+const products = require('./products.json');
+const carts = require('./carts.json');
+const ProductManager = require('./productManagement');
+
 const productManager = new ProductManager();
 
+// Retrieve all products
 app.get('/products', (req, res) => {
-    const limit = req.query.limit; // Get the value of the "limit" query parameter
-
-    // Read the products using the `getProducts()` method from the `ProductManager` instance
-    const products = productManager.getProducts();
-
-    // Check if a limit was provided
-    if (limit) {
-        const limitedProducts = products.slice(0, limit); // Get only the specified number of products
-        res.json(limitedProducts);
-    } else {
-        res.json(products);
-    }
+    res.json(products);
 });
 
 app.get('/products/:pid', (req, res) => {
-    const productId = parseInt(req.params.pid); // Get the product ID from req.params as an integer
+    const productId = parseInt(req.params.pid);
 
     // Get the requested product using the getProductById() method from the ProductManager instance
     const product = productManager.getProductById(productId);
@@ -36,6 +30,102 @@ app.get('/products/:pid', (req, res) => {
     }
 });
 
+// Add a new product
+app.post('/products', (req, res) => {
+    const newProduct = req.body;
+    productManager.addProduct(newProduct);
+    res.status(201).json(newProduct);
+});
+
+// Update a product
+app.put('/products/:pid', (req, res) => {
+    const productId = req.params.pid;
+    const updatedProduct = req.body;
+
+    const productIndex = products.findIndex((product) => product.id === productId);
+    if (productIndex !== -1) {
+        products[productIndex] = { ...products[productIndex], ...updatedProduct };
+        res.json(products[productIndex]);
+    } else {
+        res.status(404).json({ error: 'Product not found' });
+    }
+});
+
+// Delete a product
+app.delete('/products/:pid', (req, res) => {
+    const productId = req.params.pid;
+    const deleted = productManager.deleteProduct(productId);
+
+    if (deleted) {
+        res.json({ message: 'Product deleted successfully' });
+    } else {
+        res.status(404).json({ error: 'Product not found' });
+    }
+});
+
+// Retrieve all carts
+app.get('/carts', (req, res) => {
+    const allCarts = productManager.getAllCarts();
+    res.json(allCarts);
+});
+
+// Add a new cart
+app.post('/carts', (req, res) => {
+    const newCart = req.body;
+    productManager.createCart(newCart);
+    res.status(201).json(newCart);
+});
+
+// Retrieve a cart by ID
+app.get('/carts/:cid', (req, res) => {
+    const cartId = parseInt(req.params.cid);
+    const cart = productManager.getCartById(cartId);
+
+    if (cart) {
+        const cartProducts = cart.products;
+        res.json(cartProducts);
+    } else {
+        res.status(404).json({ error: 'Cart not found' });
+    }
+});
+
+// Add a product to a cart
+app.post('/carts/:cid/products/:pid', (req, res) => {
+    const cartId = parseInt(req.params.cid);
+    const productId = parseInt(req.params.pid);
+
+    productManager.addProductToCart(cartId, productId);
+    res.json({ message: 'Product added to cart successfully' });
+});
+
+// Update a cart
+app.put('/carts/:cid', (req, res) => {
+    const cartId = req.params.cid;
+    const updatedCart = req.body;
+
+    const cartIndex = carts.findIndex((cart) => cart.id === cartId);
+    if (cartIndex !== -1) {
+        carts[cartIndex] = { ...carts[cartIndex], ...updatedCart };
+        res.json(carts[cartIndex]);
+    } else {
+        res.status(404).json({ error: 'Cart not found' });
+    }
+});
+
+// Delete a cart
+app.delete('/carts/:cid', (req, res) => {
+    const cartId = req.params.cid;
+
+    const cartIndex = carts.findIndex((cart) => cart.id === cartId);
+    if (cartIndex !== -1) {
+        const deletedCart = carts.splice(cartIndex, 1);
+        res.json(deletedCart[0]);
+    } else {
+        res.status(404).json({ error: 'Cart not found' });
+    }
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
--- src/productManagement.js
+++ src/productManagement.js
@@ -4,191 +4,123 @@ const path = require('path');
 class ProductManager {
     constructor() {
         this.products = [];
+        this.carts = [];
         this.lastProductId = 0;
-        this.path = path.join(__dirname, 'products.json');
+        this.lastCartId = 0;
+        this.productsPath = path.join(__dirname, 'products.json');
+        this.cartsPath = path.join(__dirname, 'carts.json');
+        this.loadProducts();
+        this.loadCarts();
     }
 
-    addProduct(product) {
-        if (!product.code || !product.title || !product.description || !product.price || !product.thumbnail || !product.stock) {
-            console.log('All fields are mandatory.');
-            return;
+    loadProducts() {
+        try {
+            const productsData = fs.readFileSync(this.productsPath, 'utf8');
+            this.products = JSON.parse(productsData) || [];
+            this.lastProductId = this.products.length > 0 ? this.products[this.products.length - 1].id : 0;
+        } catch (error) {
+            console.log('Error loading products:', error);
         }
+    }
 
-        const existingProduct = this.products.find(p => p.code === product.code);
-        if (existingProduct) {
-            console.log('Product with the same code already exists.');
-            return;
+    saveProducts() {
+        try {
+            fs.writeFileSync(this.productsPath, JSON.stringify(this.products, null, 2));
+        } catch (error) {
+            console.log('Error saving products:', error);
         }
+    }
 
-        const newProduct = {
-            ...product,
-            id: ++this.lastProductId,
-        };
 
-        this.products.push(newProduct);
+    loadCarts() {
+        try {
+            const cartsData = fs.readFileSync(this.cartsPath, 'utf8');
+            this.carts = JSON.parse(cartsData) || [];
+            this.lastCartId = this.carts.length > 0 ? this.carts[this.carts.length - 1].id : 0;
+        } catch (error) {
+            console.log('Error loading carts:', error);
+        }
+    }
 
-        fs.writeFile(this.path, JSON.stringify(this.products), (err) => {
-            if (err) {
-                console.log('Error saving products:', err);
-            } else {
-                console.log('Product added successfully.');
-            }
-        });
+    saveCarts() {
+        try {
+            fs.writeFileSync(this.cartsPath, JSON.stringify(this.carts, null, 2));
+        } catch (error) {
+            console.log('Error saving carts:', error);
+        }
     }
 
     getProducts() {
-        if (!fs.existsSync(this.path)) {
-            return [];
-        }
-
-        const productsData = fs.readFileSync(this.path, 'utf8');
-        if (!productsData) {
-            return [];
-        }
-
-        const products = JSON.parse(productsData);
-        this.products = products; 
-        return products;
+        return this.products;
     }
 
     getProductById(productId) {
-        const product = this.products.find(product => product.id === productId);
-        if (product) {
-            return product;
-        } else {
-            console.log('Product not found.');
-        }
+        return this.products.find((product) => product.id === productId);
     }
 
-    updateProduct(productId, field, value) {
-        const productIndex = this.products.findIndex(product => product.id === productId);
-        if (productIndex === -1) {
-            console.log('Product not found.');
-            return;
+    addProduct(product) {
+        this.lastProductId++;
+        product.id = this.lastProductId;
+        this.products.push(product);
+        this.saveProducts();
+    }
+
+    
+
+    updateProduct(productId, updatedFields) {
+        const product = this.getProductById(productId);
+
+        if (product) {
+            Object.assign(product, updatedFields);
+            this.saveProducts();
         }
-
-        this.products[productIndex][field] = value;
-
-        fs.writeFile(this.path, JSON.stringify(this.products), (err) => {
-            if (err) {
-                console.log('Error updating product:', err);
-            } else {
-                console.log('Product updated successfully.');
-            }
-        });
     }
 
     deleteProduct(productId) {
-        const productIndex = this.products.findIndex(product => product.id === productId);
-        if (productIndex === -1) {
-            console.log('Product not found.');
-            return;
+        const productIndex = this.products.findIndex((product) => product.id === Number(productId));
+
+        if (productIndex !== -1) {
+            this.products.splice(productIndex, 1);
+            this.saveProducts();
+            return true; // Return true to indicate successful deletion
         }
 
-        const deletedProduct = this.products.splice(productIndex, 1)[0];
+        return false; // Return false to indicate product not found
+    }
 
-        try {
-            fs.writeFileSync(this.path, JSON.stringify(this.products));
-            console.log('Product deleted successfully:', deletedProduct);
-        } catch (err) {
-            console.log('Error deleting product:', err);
+    createCart(cart) {
+        this.lastCartId++;
+        cart.id = this.lastCartId;
+        cart.products = [];
+        this.carts.push(cart);
+        this.saveCarts();
+    }
+
+    getAllCarts() {
+        return this.carts;
+    }
+
+    getCartById(cartId) {
+        return this.carts.find((cart) => cart.id === cartId);
+    }
+
+    addProductToCart(cartId, productId) {
+        const cart = this.getCartById(cartId);
+        const product = this.getProductById(parseInt(productId));
+
+        if (cart && product) {
+            const existingProduct = cart.products.find((p) => p.id === productId);
+
+            if (existingProduct) {
+                existingProduct.quantity++;
+            } else {
+                const newProduct = { id: product.id, name: product.name, quantity: 1 };
+                cart.products.push(newProduct);
+            }
+
+            this.saveCarts();
         }
     }
 }
-
-const manager = new ProductManager();
-
-manager.addProduct({
-    code: 'P1',
-    title: 'Phone Case',
-    description: 'Protective phone case for iPhone and Android smartphones',
-    price: 9.99,
-    thumbnail: 'product1.jpg',
-    stock: 10
-});
-
-manager.addProduct({
-    code: 'P2',
-    title: 'Wireless Earbuds',
-    description: 'High-quality wireless earbuds for music and calls',
-    price: 19.99,
-    thumbnail: 'product2.jpg',
-    stock: 5
-});
-
-manager.addProduct({
-    code: 'P3',
-    title: 'Charging Cable',
-    description: 'Durable and fast-charging USB-C charging cable',
-    price: 29.99,
-    thumbnail: 'product3.jpg',
-    stock: 3
-});
-
-manager.addProduct({
-    code: 'P4',
-    title: 'Bluetooth Speaker',
-    description: 'Portable Bluetooth speaker with excellent sound quality',
-    price: 39.99,
-    thumbnail: 'product4.jpg',
-    stock: 8
-});
-
-manager.addProduct({
-    code: 'P5',
-    title: 'Smartwatch',
-    description: 'Feature-packed smartwatch for fitness and notifications',
-    price: 79.99,
-    thumbnail: 'product5.jpg',
-    stock: 2
-});
-
-manager.addProduct({
-    code: 'P6',
-    title: 'Gaming Mouse',
-    description: 'Ergonomic gaming mouse with customizable buttons',
-    price: 49.99,
-    thumbnail: 'product6.jpg',
-    stock: 6
-});
-
-manager.addProduct({
-    code: 'P7',
-    title: 'Portable Power Bank',
-    description: 'High-capacity power bank for charging devices on the go',
-    price: 24.99,
-    thumbnail: 'product7.jpg',
-    stock: 12
-});
-
-manager.addProduct({
-    code: 'P8',
-    title: 'Bluetooth Headphones',
-    description: 'Wireless Bluetooth headphones with noise cancellation',
-    price: 69.99,
-    thumbnail: 'product8.jpg',
-    stock: 4
-});
-
-manager.addProduct({
-    code: 'P9',
-    title: 'Wireless Mouse',
-    description: 'Wireless mouse with precise tracking and long battery life',
-    price: 19.99,
-    thumbnail: 'product9.jpg',
-    stock: 9
-});
-
-manager.addProduct({
-    code: 'P10',
-    title: 'External Hard Drive',
-    description: 'Portable external hard drive for data backup and storage',
-    price: 89.99,
-    thumbnail: 'product10.jpg',
-    stock: 7
-});
-
-
-manager.updateProduct(2, 'title', 'Updated Wireless Earbuds');
 
 module.exports = ProductManager;
