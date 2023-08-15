87e4eb162fe6ff9818c9b34fa5db5cf2d694a304
Jhenry Sierra <jhenrims@gmail.com>
2023-08-15 19:55:46 -0400

    implemented Passport Github Strategy

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
-const User = require('../daos/mongodb/models/user.model'); // Import the User model
+const User = require('../daos/mongodb/models/user.model');
 const isAuthenticated = require('../middlewares/isAuthenticated');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const router = express.Router();
 
@@ -32,8 +33,6 @@ passport.use(new LocalStrategy(
     }
 ));
 
-
-
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
@@ -47,6 +46,49 @@ passport.deserializeUser(async (id, done) => {
     }
 });
 
+//Passport Github strategy
+passport.use(new GitHubStrategy({
+    clientID: 'Iv1.90fd71a6cca5ab8e',
+    clientSecret: 'e7e4db8d56ffbda5382b80c408cecc590be39e5b',
+    callbackURL: "http://localhost:8080/auth/github/callback" // Use your callback URL
+},
+    async (accessToken, refreshToken, profile, done) => {
+        try {
+            // Check if the user exists in your database based on the GitHub email
+            const existingUser = await User.findOne({ email: profile.emails[0].value });
+
+            if (existingUser) {
+                // If user exists, return the user
+                return done(null, existingUser);
+            }
+
+            // If user doesn't exist, create a new user in your database
+            const newUser = new User({
+                email: profile.emails[0].value || 'default@example.com', 
+                username: profile.username || 'defaultUsername', 
+                password: 'defaultPassword', 
+                role: 'user'
+            });
+
+            await newUser.save();
+            return done(null, newUser);
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
+
+router.get('/github', passport.authenticate('github', {scope: ['user:email']}));
+
+// GitHub callback route
+router.get('/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful authentication, redirect to a secure route or send a response
+        res.redirect('/products');
+    }
+);
+//----------------end github strategy-------------//
 
 // Route to render the registration form view
 router.get('/register', (req, res) => {
@@ -95,8 +137,9 @@ router.post('/login', (req, res) => {
 
 // Route to handle user logout
 router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/');
+    req.logout(() => {
+        res.redirect('/auth/login');
+    });
 });
 
 module.exports = router;
